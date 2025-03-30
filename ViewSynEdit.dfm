0225c6278829a9a1a3aabf6491fe2043baf6e0d9
fuyi4it <fuyi.it@live.cn>
2025-03-30 13:45:40 +0800

    已经完成文件高亮显示

--- ViewSynEdit.dfm
+++ ViewSynEdit.dfm
@@ -3,7 +3,7 @@ object Form2: TForm2
   Top = 0
   Caption = #25991#20214#20869#23481#26597#30475#22120
   ClientHeight = 844
-  ClientWidth = 1191
+  ClientWidth = 690
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -16,13 +16,13 @@ object Form2: TForm2
   object Panel1: TPanel
     Left = 0
     Top = 0
-    Width = 1191
+    Width = 690
     Height = 41
     Align = alTop
     TabOrder = 0
-    ExplicitWidth = 618
+    ExplicitWidth = 1191
     DesignSize = (
-      1191
+      690
       41)
     object lblFileInfo: TLabel
       Left = 16
@@ -33,7 +33,7 @@ object Form2: TForm2
       Caption = #25991#20214#20449#24687
     end
     object btnClose: TButton
-      Left = 1080
+      Left = 579
       Top = 10
       Width = 89
       Height = 25
@@ -41,12 +41,13 @@ object Form2: TForm2
       Caption = #20851#38381
       TabOrder = 0
       OnClick = btnCloseClick
+      ExplicitLeft = 1080
     end
   end
   object SynEdit1: TSynEdit
     Left = 0
     Top = 41
-    Width = 1191
+    Width = 690
     Height = 803
     Align = alClient
     Font.Charset = DEFAULT_CHARSET
@@ -54,6 +55,7 @@ object Form2: TForm2
     Font.Height = -13
     Font.Name = 'Consolas'
     Font.Style = []
+    Font.Quality = fqClearTypeNatural
     TabOrder = 1
     UseCodeFolding = False
     Gutter.Font.Charset = DEFAULT_CHARSET
@@ -61,14 +63,31 @@ object Form2: TForm2
     Gutter.Font.Height = -11
     Gutter.Font.Name = 'Courier New'
     Gutter.Font.Style = []
+    Gutter.Font.Quality = fqClearTypeNatural
     Gutter.ShowLineNumbers = True
-    Gutter.Width = 30
-    Highlighter = nil
+    Gutter.Bands = <
+      item
+        Kind = gbkMarks
+        Width = 13
+      end
+      item
+        Kind = gbkLineNumbers
+      end
+      item
+        Kind = gbkFold
+      end
+      item
+        Kind = gbkTrackChanges
+      end
+      item
+        Kind = gbkMargin
+        Width = 3
+      end>
     Lines.Strings = (
       '')
     Options = [eoAutoIndent, eoDragDropEditing, eoEnhanceEndKey, eoGroupUndo, eoScrollPastEol, eoShowScrollHint, eoSmartTabDelete, eoTabIndent, eoTabsToSpaces]
+    SelectedColor.Alpha = 0.400000005960464500
     TabWidth = 2
-    WantTabs = False
-    ExplicitHeight = 383
+    ExplicitWidth = 1191
   end
 end
--- ViewSynEdit.pas
+++ ViewSynEdit.pas
@@ -5,7 +5,12 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
-  System.IOUtils, System.Math, SynEdit, SynEditHighlighter, SynHighlighterXML, SynHighlighterHTML;
+  System.IOUtils, System.Math, SynEdit, SynEditHighlighter,
+  SynHighlighterXML, SynHighlighterHTML, SynHighlighterPas, SynHighlighterJava,
+  SynHighlighterCpp, SynHighlighterPython, SynHighlighterCS, SynHighlighterSQL,
+  SynHighlighterJScript, SynHighlighterCSS, SynHighlighterJSON, SynHighlighterIni,
+  SynHighlighterBat, SynHighlighterVB, SynHighlighterPerl, SynHighlighterPHP,
+  SynHighlighterTeX, SynHighlighterRuby;
 
 type
   TForm2 = class(TForm)
@@ -19,7 +24,24 @@ type
     { Private declarations }
     FHighlighterXML: TSynXMLSyn;
     FHighlighterHTML: TSynHTMLSyn;
+    FHighlighterPas: TSynPasSyn;
+    FHighlighterJava: TSynJavaSyn;
+    FHighlighterCpp: TSynCppSyn;
+    FHighlighterPython: TSynPythonSyn;
+    FHighlighterCS: TSynCSSyn;
+    FHighlighterSQL: TSynSQLSyn;
+    FHighlighterJS: TSynJScriptSyn;
+    FHighlighterCSS: TSynCssSyn;
+    FHighlighterJSON: TSynJSONSyn;
+    FHighlighterIni: TSynIniSyn;
+    FHighlighterBat: TSynBatSyn;
+    FHighlighterVB: TSynVBSyn;
+    FHighlighterPerl: TSynPerlSyn;
+    FHighlighterPHP: TSynPHPSyn;
+    FHighlighterTeX: TSynTeXSyn;
+    FHighlighterRuby: TSynRubySyn;
     procedure ApplyHighlighter(const FileName: string);
+    procedure CheckHighlightersStatus;
   public
     { Public declarations }
     procedure LoadFile(const FileName: string; Encoding: TEncoding);
@@ -40,19 +62,56 @@ end;
 
 procedure TForm2.FormCreate(Sender: TObject);
 begin
-  // 初始化语法高亮
-  FHighlighterXML := TSynXMLSyn.Create(Self);
-  FHighlighterHTML := TSynHTMLSyn.Create(Self);
-  
-  // 设置SynEdit基本属性
-  SynEdit1.Gutter.ShowLineNumbers := True;
-  SynEdit1.Font.Name := 'Consolas';
-  SynEdit1.Font.Size := 10;
-  SynEdit1.Options := SynEdit1.Options + [eoTabsToSpaces];
-  SynEdit1.TabWidth := 2;
-  
-  // 设置窗体属性
-  Position := poScreenCenter;
+  try
+    // 初始化语法高亮器
+    FHighlighterXML := TSynXMLSyn.Create(Self);
+    FHighlighterHTML := TSynHTMLSyn.Create(Self);
+    FHighlighterPas := TSynPasSyn.Create(Self);
+    
+    // 特别确保Pascal高亮器正确配置
+    if Assigned(FHighlighterPas) then
+    begin
+      FHighlighterPas.KeyAttri.Foreground := clBlue;
+      FHighlighterPas.StringAttri.Foreground := clRed;
+      FHighlighterPas.CommentAttri.Foreground := clGreen;
+      FHighlighterPas.NumberAttri.Foreground := clMaroon;
+      OutputDebugString(PChar('Pascal高亮器初始化成功'));
+    end
+    else
+      OutputDebugString(PChar('警告: Pascal高亮器创建失败'));
+    
+    FHighlighterJava := TSynJavaSyn.Create(Self);
+    FHighlighterCpp := TSynCppSyn.Create(Self);
+    FHighlighterPython := TSynPythonSyn.Create(Self);
+    FHighlighterCS := TSynCSSyn.Create(Self);
+    FHighlighterSQL := TSynSQLSyn.Create(Self);
+    FHighlighterJS := TSynJScriptSyn.Create(Self);
+    FHighlighterCSS := TSynCssSyn.Create(Self);
+    FHighlighterJSON := TSynJSONSyn.Create(Self);
+    FHighlighterIni := TSynIniSyn.Create(Self);
+    FHighlighterBat := TSynBatSyn.Create(Self);
+    FHighlighterVB := TSynVBSyn.Create(Self);
+    FHighlighterPerl := TSynPerlSyn.Create(Self);
+    FHighlighterPHP := TSynPHPSyn.Create(Self);
+    FHighlighterTeX := TSynTeXSyn.Create(Self);
+    FHighlighterRuby := TSynRubySyn.Create(Self);
+    
+    // 设置SynEdit基本属性
+    SynEdit1.Gutter.ShowLineNumbers := True;
+    SynEdit1.Font.Name := 'Consolas';
+    SynEdit1.Font.Size := 10;
+    SynEdit1.Options := SynEdit1.Options + [eoTabsToSpaces];
+    SynEdit1.TabWidth := 2;
+    
+    // 设置窗体属性
+    Position := poScreenCenter;
+    
+    // 检查高亮器状态
+    CheckHighlightersStatus;
+  except
+    on E: Exception do
+      ShowMessage('初始化高亮器失败: ' + E.Message);
+  end;
 end;
 
 procedure TForm2.ApplyHighlighter(const FileName: string);
@@ -64,11 +123,107 @@ begin
   // 先清除当前高亮
   SynEdit1.Highlighter := nil;
   
-  // 根据文件扩展名应用对应的语法高亮
-  if (Ext = '.xml') or (Ext = '.svg') or (Ext = '.config') then
+  // 调试信息
+  OutputDebugString(PChar('正在为文件应用高亮: ' + FileName + ' 扩展名: ' + Ext));
+  
+  // Delphi/Pascal
+  if (Ext = '.pas') or (Ext = '.dpr') or (Ext = '.dpk') or (Ext = '.inc') 
+    or (Ext = '.dfm') or (Ext = '.fmx') then
+  begin
+    SynEdit1.Highlighter := FHighlighterPas;
+    OutputDebugString(PChar('应用Pascal高亮器'));
+    if FHighlighterPas = nil then
+      OutputDebugString(PChar('警告: Pascal高亮器为nil'));
+  end
+    
+  // XML相关
+  else if (Ext = '.xml') or (Ext = '.svg') or (Ext = '.config') or (Ext = '.xsd') 
+    or (Ext = '.xsl') or (Ext = '.xslt') or (Ext = '.dtd') or (Ext = '.resx') 
+    or (Ext = '.manifest') then
     SynEdit1.Highlighter := FHighlighterXML
-  else if (Ext = '.html') or (Ext = '.htm') then
-    SynEdit1.Highlighter := FHighlighterHTML;
+    
+  // HTML相关
+  else if (Ext = '.html') or (Ext = '.htm') or (Ext = '.shtml') or (Ext = '.xhtml') then
+    SynEdit1.Highlighter := FHighlighterHTML
+    
+  // Java
+  else if (Ext = '.java') or (Ext = '.jav') then
+    SynEdit1.Highlighter := FHighlighterJava
+    
+  // C/C++
+  else if (Ext = '.c') or (Ext = '.cpp') or (Ext = '.cc') or (Ext = '.h') 
+    or (Ext = '.hpp') or (Ext = '.hh') or (Ext = '.cxx') or (Ext = '.hxx') then
+    SynEdit1.Highlighter := FHighlighterCpp
+    
+  // Python
+  else if (Ext = '.py') or (Ext = '.pyw') or (Ext = '.pyc') or (Ext = '.pyd') 
+    or (Ext = '.pyo') then
+    SynEdit1.Highlighter := FHighlighterPython
+    
+  // C#
+  else if (Ext = '.cs') then
+    SynEdit1.Highlighter := FHighlighterCS
+    
+  // SQL
+  else if (Ext = '.sql') or (Ext = '.ddl') or (Ext = '.dml') or (Ext = '.pls') 
+    or (Ext = '.pks') or (Ext = '.pkb') then
+    SynEdit1.Highlighter := FHighlighterSQL
+    
+  // JavaScript
+  else if (Ext = '.js') or (Ext = '.jsx') or (Ext = '.mjs') then
+    SynEdit1.Highlighter := FHighlighterJS
+    
+  // CSS
+  else if (Ext = '.css') or (Ext = '.scss') or (Ext = '.sass') or (Ext = '.less') then
+    SynEdit1.Highlighter := FHighlighterCSS
+    
+  // JSON
+  else if (Ext = '.json') then
+    SynEdit1.Highlighter := FHighlighterJSON
+    
+  // INI files
+  else if (Ext = '.ini') or (Ext = '.inf') or (Ext = '.conf') or (Ext = '.cfg') 
+    or (Ext = '.properties') then
+    SynEdit1.Highlighter := FHighlighterIni
+    
+  // Batch files
+  else if (Ext = '.bat') or (Ext = '.cmd') then
+    SynEdit1.Highlighter := FHighlighterBat
+    
+  // Visual Basic
+  else if (Ext = '.vb') or (Ext = '.bas') or (Ext = '.vbs') or (Ext = '.frm') 
+    or (Ext = '.cls') then
+    SynEdit1.Highlighter := FHighlighterVB
+    
+  // Perl
+  else if (Ext = '.pl') or (Ext = '.pm') or (Ext = '.perl') then
+    SynEdit1.Highlighter := FHighlighterPerl
+    
+  // PHP
+  else if (Ext = '.php') or (Ext = '.php3') or (Ext = '.php4') or (Ext = '.php5') 
+    or (Ext = '.phtml') then
+    SynEdit1.Highlighter := FHighlighterPHP
+    
+  // TeX/LaTeX
+  else if (Ext = '.tex') or (Ext = '.latex') or (Ext = '.sty') or (Ext = '.cls') 
+    or (Ext = '.ltx') then
+    SynEdit1.Highlighter := FHighlighterTeX
+    
+  // Ruby
+  else if (Ext = '.rb') or (Ext = '.rbw') or (Ext = '.rake') or (Ext = '.gemspec') then
+    SynEdit1.Highlighter := FHighlighterRuby;
+  
+  // 为标题添加语言信息
+  if SynEdit1.Highlighter <> nil then
+  begin
+    Caption := ExtractFileName(FileName) + ' - [' + SynEdit1.Highlighter.LanguageName + ']';
+    OutputDebugString(PChar('成功应用高亮器: ' + SynEdit1.Highlighter.LanguageName));
+  end
+  else
+  begin
+    Caption := ExtractFileName(FileName);
+    OutputDebugString(PChar('未应用任何高亮器'));
+  end;
 end;
 
 procedure TForm2.LoadFile(const FileName: string; Encoding: TEncoding);
@@ -81,11 +236,16 @@ var
   Buffer: TBytes;
   NonTextChars: Integer;
   i: Integer;
+  Ext: string;
 begin
   try
     // 检查文件是否存在
     if not FileExists(FileName) then
       raise Exception.Create('文件不存在');
+      
+    // 获取文件扩展名
+    Ext := LowerCase(ExtractFileExt(FileName));
+    OutputDebugString(PChar('加载文件: ' + FileName + ' 扩展名: ' + Ext));
       
     Stream := TFileStream.Create(FileName, fmOpenRead or fmShareDenyNone);
     try
@@ -130,6 +290,18 @@ begin
         // 应用语法高亮
         ApplyHighlighter(FileName);
         
+        // 特殊处理Pascal文件
+        if (Ext = '.pas') and (SynEdit1.Highlighter = nil) then
+        begin
+          OutputDebugString(PChar('特殊处理Pascal文件'));
+          if Assigned(FHighlighterPas) then
+          begin
+            SynEdit1.Highlighter := FHighlighterPas;
+            Caption := ExtractFileName(FileName) + ' - [Object Pascal]';
+            OutputDebugString(PChar('手动设置Pascal高亮器'));
+          end;
+        end;
+        
         // 检查内容是否包含大量乱码
         NonTextChars := 0;
         for i := 1 to System.Math.Min(Length(Content), 1000) do
@@ -153,7 +325,8 @@ begin
     end;
     
     // 设置窗口标题
-    Caption := ExtractFileName(FileName);
+    if SynEdit1.Highlighter = nil then
+      Caption := ExtractFileName(FileName);
   except
     on E: Exception do
       raise Exception.Create('加载文件失败: ' + E.Message);
@@ -180,6 +353,54 @@ begin
   // 设置信息标签
   lblFileInfo.Caption := Format('文件: %s | 大小: %d 字节 | 编码: %s (%s) | 行数: %d', 
     [ExtractFileName(FileName), FileSize, EncodingName, BOMStr, SynEdit1.Lines.Count]);
+end;
+
+procedure TForm2.CheckHighlightersStatus;
+var
+  Log: string;
+begin
+  Log := 'SynEdit组件状态:';
+  if Assigned(SynEdit1) then
+    Log := Log + ' SynEdit正常'
+  else
+    Log := Log + ' SynEdit未正确加载!';
+    
+  Log := Log + #13#10 + '高亮器状态:';
+  if Assigned(FHighlighterPas) then
+    Log := Log + ' Pascal高亮器正常'
+  else
+    Log := Log + ' Pascal高亮器未加载!';
+    
+  if Assigned(FHighlighterXML) then
+    Log := Log + ' XML高亮器正常'
+  else
+    Log := Log + ' XML高亮器未加载!';
+    
+  OutputDebugString(PChar(Log));
+  
+  // 测试基本高亮是否有效
+  try
+    // 测试Pascal高亮器
+    if Assigned(FHighlighterPas) then
+    begin
+      SynEdit1.Lines.Clear;
+      SynEdit1.Lines.Add('procedure Test;');
+      SynEdit1.Lines.Add('begin');
+      SynEdit1.Lines.Add('  ShowMessage(''Hello'');');
+      SynEdit1.Lines.Add('end;');
+      
+      SynEdit1.Highlighter := FHighlighterPas;
+      Application.ProcessMessages;
+      
+      if SynEdit1.Highlighter = FHighlighterPas then
+        OutputDebugString(PChar('Pascal高亮器应用成功'))
+      else
+        OutputDebugString(PChar('警告: Pascal高亮器应用失败!'));
+    end;
+  except
+    on E: Exception do
+      OutputDebugString(PChar('测试高亮器时出错: ' + E.Message));
+  end;
 end;
 
 end. 
